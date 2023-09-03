f55689c967bb762f69fae4b8985bce48c7d1342a
siam-craft <abdullahsiam009@gmail.com>
2023-09-03 18:17:58 +0600

    dummy banner component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBox from '../components/SearchBox';
 import AllCapsules from '../components/AllCapsules';
+import Banner from '../components/Banner';
 
 const Home = () => (
   <>
-    <div className="bg-indigo-600 px-10 py-9 h-[30vh]  w-full" />
+    {/* <div className="bg-indigo-600 px-10 py-9 h-[30vh]  w-full" /> */}
+    <Banner />
     <SearchBox />
     <AllCapsules />
   </>
