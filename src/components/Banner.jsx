095d76d51cfae4e9c95960de5ea74e1a699a0097
siam-craft <abdullahsiam009@gmail.com>
2023-09-03 22:55:34 +0600

    bug fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Banner = () => (
   <div className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white py-16">
     <div className="container mx-auto text-center">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Our API</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to Rocket Travel</h1>
       <p className="text-lg mb-8">
-        Explore and access the data you need with our powerful API.
+        Pursue your dream and fly through our ROCKETS
       </p>
       <button
         type="button"
